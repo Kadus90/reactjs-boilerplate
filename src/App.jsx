b86679ec5039d786d99c14d92815e27f9aa418f6
Julian Phillips <julian@julianlp.com>
2021-08-31 15:32:17 -0600

    feat(react-router-dom): implemented and moved content to first page file

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Index from './pages/index';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function App() {
   return (
     <div className="App">
-      <h1>React Boilerplate</h1>
-      <h2>Completed Initial Features</h2>
-      <h3>Dev Dependencies</h3>
-      <ul>
-        <li>Webpack</li>
-        <li>ESLint</li>
-        <li>Prettier</li>
-        <li>Husky</li>
-        <li>Lint-Staged</li>
-        <li>Commitlint</li>
-      </ul>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Index = () => (
+  <div>
+    <h1>React Boilerplate</h1>
+    <h2>Completed Initial Features</h2>
+    <h3>Dev Dependencies</h3>
+    <ul>
+      <li>Webpack</li>
+      <li>ESLint</li>
+      <li>Prettier</li>
+      <li>Husky</li>
+      <li>Lint-Staged</li>
+      <li>Commitlint</li>
+    </ul>
+  </div>
+);
+
+export default Index;
