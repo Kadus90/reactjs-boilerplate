63a1d90e9eb3f60995fd82459c8de7cf5ba59c61
Julian Phillips <julian@julianlp.com>
2021-08-31 11:50:36 -0600

    added extensions to resolve, chunked bundle, added compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,69 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: '[name].[chunkhash].bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: [path.join(__dirname, 'src'), 'node_modules'],
+    alias: {
+      react: path.join(__dirname, 'node_modules', 'react'),
     },
-    resolve: {
-        modules: [path.join(__dirname, 'src'), 'node_modules'],
-        alias: {
-            react: path.join(__dirname, 'node_modules', 'react'),
-        }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+    }),
+    new CompressionPlugin({
+      test: /\.js(\?.*)?$/i,
+    }),
+  ],
+  performance: {
+    hints: 'warning',
+    assetFilter(assetFilename) {
+      return assetFilename.endsWith('.js.gz');
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader',
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ]
-            }
-        ]
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          test: /node_modules\/*/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
     },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+    runtimeChunk: {
+      name: 'manifest',
+    },
+  },
+};
