4bb7496fea4749dc27be47573afd2bf926aa610e
Julian Phillips <julian@julianlp.com>
2021-08-31 14:59:18 -0600

    feat: added list of features to App page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 function App() {
-    return (
-        <div className="App">
-            <h3>Hello World</h3>
-        </div>
-    )
-};
+  return (
+    <div className="App">
+      <h1>React Boilerplate</h1>
+      <h2>Completed Initial Features</h2>
+      <h3>Dev Dependencies</h3>
+      <ul>
+        <li>Webpack</li>
+        <li>ESLint</li>
+        <li>Prettier</li>
+        <li>Husky</li>
+        <li>Lint-Staged</li>
+        <li>Commitlint</li>
+      </ul>
+    </div>
+  );
+}
 
 export default App;
