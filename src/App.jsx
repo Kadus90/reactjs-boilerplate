e0011a8556c707ba4f72f20be2c0e64fa9195cb0
Julian Phillips <julian@julianlp.com>
2021-08-31 16:16:00 -0600

    refactor: adjusted syntax

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Index from './pages/index';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Index />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Index />
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
