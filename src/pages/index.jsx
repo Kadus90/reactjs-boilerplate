76e98ea00a787dfb18827ff700eab17c3b6f6135
Julian Phillips <julian@julianlp.com>
2021-08-31 16:12:43 -0600

    docs: added to list

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,11 @@ const Index = () => (
   <div>
     <h1>React Boilerplate</h1>
     <h2>Completed Initial Features</h2>
-    <h3>Dev Dependencies</h3>
+    <h3>Front End</h3>
+    <ul>
+      <li>Page routing via react-router-dom</li>
+    </ul>
+    <h3>Dev Dependency Configuration</h3>
     <ul>
       <li>Webpack</li>
       <li>ESLint</li>
