af410456b7094b411c86eff91de81390f5c53075
Julian Phillips <julian@julianlp.com>
2021-08-31 15:13:54 -0600

    refactor: switched from extracting render to using ReactDOM and applied StrictMode

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,11 @@
-// Test for precommit hook for formatting
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import App from './App';
 
-render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
