f44e1b6d1b8e018770475c3b7c7810fb13efd90a
Julian Phillips <julian@julianlp.com>
2021-08-31 22:58:24 -0600

    feat(styles): installed styled-components and implemented an example

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import React, {useContext} from 'react';
+
 import DevConfigContext from '../contexts/devConfigContext';
+import {PageWrapper} from '../components/Page/Page.styles';
 
 const Index = () => {
   const devConfig = useContext(DevConfigContext);
@@ -8,7 +10,7 @@ const Index = () => {
   const renderDevConfigList = (configItem) => configItem.map((item, i) => <li key={`devConfigItem${i}`}>{item}</li>);
 
   return (
-    <div>
+    <PageWrapper>
       <h1>React Boilerplate</h1>
       <h2>Completed Initial Features</h2>
       <h3>Front End</h3>
@@ -16,10 +18,11 @@ const Index = () => {
         <li>Page routing via react-router-dom</li>
         <li>Context example</li>
         <li>Base styling</li>
+        <li>Styled-Components</li>
       </ul>
       <h3>Dev Dependency Configuration</h3>
       <ul>{renderDevConfigList.length > 0 && renderDevConfigList(devConfig)}</ul>
-    </div>
+    </PageWrapper>
   );
 };
 
